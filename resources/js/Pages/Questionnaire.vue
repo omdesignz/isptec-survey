6b9d7fe09faa0ddc4e7a4467534928d6d5b45c81
Osvaldo Manuel <veneravel.osvaldo@gmail.com>
2022-01-27 10:39:59 +0100

    Make main screen full height

--- resources/js/Pages/Questionnaire.vue
+++ resources/js/Pages/Questionnaire.vue
@@ -1,6 +1,6 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="relative bg-gray-800">
+  <div class="relative bg-gray-800 min-h-screen">
     <div class="h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
       <img class="w-full h-full object-cover" src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply" alt="" />
     </div>
